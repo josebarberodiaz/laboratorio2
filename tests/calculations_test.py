f1036eb830d6ae4f3ff77e655e0ea27557c125c5
josebarberodiaz <54626565+josebarberodiaz@users.noreply.github.com>
2026-06-01 10:55:54 +0000

    test2

--- tests/calculations_test.py
+++ tests/calculations_test.py
@@ -59,10 +59,12 @@ def test_get_nth_fibonacci_one():
 
 
 def test_get_nth_fibonacci_ten():
-     """Test with n=10."""
-     # Arrange
-     n = 10
-     # Act
-     result = get_nth_fibonacci(n)
-     # Assert
-     assert result == 89
+   """Test with n=10."""
+   # Arrange
+   n = 10
+
+   # Act
+   result = get_nth_fibonacci(n)
+
+   # Assert
+   assert result == 55
